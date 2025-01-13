a2911f228e3491a7769f17dc19a166f8e174d645
Chustin <josthinayon@gmail.com>
2025-01-13 10:20:06 -0500

    feat: setting up redux

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,11 +1,32 @@
+import MicroTestPage from 'microapp/MicroTestPage';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ThemeToggle from '~/components/ui/ThemeTogle';
+import { RootState, login } from '~/store';
 
 const Main: React.FC = () => {
+  const { isAuthenticated, token } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+
+  const handleDispatch = () => {
+    dispatch(
+      login({
+        token: 'dada',
+        isAuthenticated: true,
+        username: 'Pepe',
+      })
+    );
+  };
+
   return (
-    <div className="bg-background-light dark:bg-background-dark text-font-light dark:text-font-dark min-h-screen p-4">
-      <h1 className="text-2xl">Modo Noche y Día con TailwindCSS</h1>
+    <div className="bg-[var(--bg-color)] min-h-screen p-4">
+      <h1 className="text-2xl text-[var(--font-color)]">Modo Noche y Día con TailwindCSS</h1>
+      {`${isAuthenticated}`}
+      {`${token}`}
       <ThemeToggle />
+      <MicroTestPage />
+      <button onClick={handleDispatch}>Test dispatch</button>
     </div>
   );
 };
