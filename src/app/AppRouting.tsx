dcb483dc6b022c0a4cbf5710f2a5edd3f01af50e
Chustin <josthinayon@gmail.com>
2025-01-16 11:18:29 -0500

    feat: sharing routing

--- src/app/AppRouting.tsx
+++ src/app/AppRouting.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { Main } from './dashboard/Main';
+
+const RemoteRouting = React.lazy(() => import('microapp/RemoteRouting'));
 
 const AppRouting: React.FC = () => {
   return (
     <Routes>
       <Route path="/home" element={<Main />} />
+      <Route
+        path="/clientes/*"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            {/* Aquí el micro frontend utiliza los contextos del host */}
+            <RemoteRouting />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 };
