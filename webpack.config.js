9e8faeabe063bec5a605a3060c94e963d68ff9b6
Chustin <josthinayon@gmail.com>
2025-01-29 10:35:51 -0500

    feat: update src path

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ module.exports = (_, argv) => {
       extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
       alias: {
         '~': path.resolve(__dirname, 'src/sasf-commons'),
+        '@': path.resolve(__dirname, 'src'),
       },
     },
 
